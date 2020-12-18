54a1311edf387ae693d6f64cf58d49b7edcf5710
Steven Brill <srbrill@stanford.edu>
2020-12-17 20:49:49 -0500

    Fix bug in rans

--- navier_stokes/get_rans_coeffs.m
+++ navier_stokes/get_rans_coeffs.m
@@ -19,7 +19,9 @@ function [mu_t,gam_k,gam_omg,G_k,G_omg,Y_k,Y_omg,S_k,S_omg] ...
     Re_t = rho.*k./(mu.*omg);
     
     alpha_star = alpha_inf_star.*(alpha_0_star+Re_t./R_k)./(1+Re_t./R_k);
+    % alpha_star can b 1
     alpha = alpha_inf./alpha_star.*(alpha_0+Re_t./R_omg)./(1+Re_t./R_omg);
+    % alpha can be 13/25
         
     
     xk = 1./(omg.^3).*(dkdx.*domgdx+dkdy.*domgdy); 
@@ -41,6 +43,7 @@ function [mu_t,gam_k,gam_omg,G_k,G_omg,Y_k,Y_omg,S_k,S_omg] ...
 
     
     beta_star = beta_inf_star.*f_beta_star.*((4/15+(Re_t./R_beta).^4)./(1+(Re_t./R_beta).^4));
+    % Last term is only in some formulations
     
     mu_t = alpha_star.*rho.*k./omg;
     gam_k = mu + mu_t./sigma_k;
@@ -48,7 +51,7 @@ function [mu_t,gam_k,gam_omg,G_k,G_omg,Y_k,Y_omg,S_k,S_omg] ...
     G_k = mu_t.*S2;
     G_omg = alpha.*omg./k.*G_k;
     Y_k = rho.*beta_star.*k.*omg;
-    Y_omg = rho.*beta.*f_beta.*omg.^2;
+    Y_omg = rho.*beta.*omg.^2;
     S_k = 0.*k;
     S_omg = 0.*k;
 
