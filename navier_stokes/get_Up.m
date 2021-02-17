9164a3f5201980cdda20532da47207e579cd3f39
Steven Brill <srbrill@stanford.edu>
2021-02-17 03:49:58 -0800

    Improve rans

--- navier_stokes/get_Up.m
+++ navier_stokes/get_Up.m
@@ -1,6 +1,6 @@
 function [Yp,Up] = get_Up(Y,U,u_tau,mu,lotw,Ey,N,psi,en_on)
 
-    U_poly = U-psi{1}(0,Y)*en_on;
+    U_poly = U-psi{1}(0,Y)*(en_on>0);
     N_sample = 1000;
     for i = 1:Ey
         basis = get_nodal_basis_coeffs(Y((N+1)*(i-1)+1:(N+1)*(i)));
@@ -10,7 +10,7 @@ function [Yp,Up] = get_Up(Y,U,u_tau,mu,lotw,Ey,N,psi,en_on)
         end
         u_sample((N_sample)*(i-1)+1:(N_sample)*(i)) = phi_sample*U_poly((N+1)*(i-1)+1:(N+1)*(i));
     end
-    u_sample = u_sample + psi{1}(0,y_sample)*en_on;
+    u_sample = u_sample + psi{1}(0,y_sample)*(en_on>0);
 
     Yp = (Y+1)*u_tau/mu;
     Up = U/u_tau;
